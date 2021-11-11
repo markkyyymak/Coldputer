9cee4b4748b8c47f95b77c019d09bbc4c85f4ebf
markkyyymak <94115658+markkyyymak@users.noreply.github.com>
2021-11-11 19:59:59 +0800

    Add files via upload

---
+++ Kattis.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Kattis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int cases = Integer.parseInt(sc.nextLine());
+        sc.nextLine();
+        System.out.println(sc.nextLine().split("-").length -1);
+    }
+}
